1db2d49d7252704a36e436a5e1a2406b274603c4
Eric Sellors <github@esellors.com>
2020-09-17 11:02:15 -0500

    final

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright 2020</p>
+            <p>Made with love by firedoggo</p>
+        </footer>
+    )
+}
+
+export default Footer;
